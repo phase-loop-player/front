e734fbc49635847e7f7a27f1f646a656886da9b0
yoyota <smarthorse6@gmail.com>
2020-03-22 19:43:12 +0900

    Implement simple loop

--- src/components/PhaseLoopPlayer.js
+++ src/components/PhaseLoopPlayer.js
@@ -1,9 +1,38 @@
-import React from "react"
+import React, { useRef, useState, useEffect } from "react"
+import ReactPlayer from "react-player"
+import { Button } from "react-bootstrap"
 
+const url = "https://www.youtube.com/watch?v=FZnZU9JInL0"
 function PhaseLoopPlayer() {
+  const [player, setPlayer] = useState()
+  const playerRef = useRef()
+  function onReady() {
+    setPlayer(playerRef.current)
+  }
+  useEffect(() => {
+    if (!player) {
+      return () => {}
+    }
+    const intervalID = setInterval(() => {
+      const currentTime = player.getCurrentTime()
+      if (currentTime > 3) {
+        player.seekTo(1.5, "seconds")
+      }
+    }, 25)
+    return () => clearInterval(intervalID)
+  }, [player])
+
   return (
     <>
-      <p>home</p>
+      <h1>Phase loop player</h1>
+      <ReactPlayer playing ref={playerRef} url={url} onReady={onReady} />
+      <Button
+        onClick={() => {
+          player.seekTo(10, "seconds")
+        }}
+      >
+        seek to
+      </Button>
     </>
   )
 }
