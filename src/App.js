af24c20a8c5fe98997b3b13ff25167fcb20fbd5b
yoyota <smarthorse6@gmail.com>
2020-04-04 15:56:46 +0900

    Add hot keys

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import React from "react"
 import { Container } from "react-bootstrap"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
-import { HotKeys } from "react-hotkeys"
 import { PhraseLoopPlayerContainer } from "./phaseLoopPlayer"
-
-const keyMap = {
-  PREVIOUS: ["j"],
-  NEXT: ["left"]
-}
 
 function App() {
   return (
-    <HotKeys keyMap={keyMap}>
+    <>
       <ToastContainer />
       <Container>
         <PhraseLoopPlayerContainer />
       </Container>
-    </HotKeys>
+    </>
   )
 }
 
