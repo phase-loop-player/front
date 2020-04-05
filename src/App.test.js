44143635b3fc4fc2f4f3c091efb4af40dc2faf77
yoyota <smarthorse6@gmail.com>
2020-04-05 11:25:46 +0900

    Refactor input form

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from "react"
 import { render } from "@testing-library/react"
 import App from "./App"
 
-test("renders home", () => {
+test("render app", () => {
   const { getByText } = render(<App />)
   const homeElement = getByText(/phase loop player/i)
   expect(homeElement).toBeInTheDocument()
--- src/phaseLoopPlayer/PhraseLoopPlayerContainer.js
+++ src/phaseLoopPlayer/PhraseLoopPlayerContainer.js
@@ -1,45 +1,48 @@
 import React, { useState, useEffect } from "react"
-import Input from "../components/Input"
-import useLocalStorage from "../hooks/useLocalStorage"
+import InputForm from "./InputForm"
 import PhraseLoopPlayer from "./PhraseLoopPlayer"
-import { backend } from "../api"
+
+function catchException(fn) {
+  try {
+    fn()
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e)
+  }
+}
 
 export default function PhraseLoopPlayerContainer() {
-  const [url, handleUrlChange] = useLocalStorage("url")
-  const [minDuration, handleMinDurationChange] = useLocalStorage("minDuration")
-  const [maxDuration, handleMaxDurationChange] = useLocalStorage("maxDuration")
+  const [url, setUrl] = useState("url")
   const [regions, setRegions] = useState()
+
   useEffect(() => {
-    async function fetchRegions() {
-      const storeKey = `${url}_${minDuration}_${maxDuration}`
-      const storedRegions = localStorage.getItem(storeKey)
-      if (storedRegions) {
-        setRegions(JSON.parse(storedRegions))
+    catchException(() => {
+      const settingsString = localStorage.getItem("settings")
+      if (!settingsString) {
         return
       }
-      const { data } = await backend.get(
-        `/regions?url=${url}&minDuration=${minDuration}&maxDuration=${maxDuration}`
-      )
-      setRegions(data.regions)
-      localStorage.setItem(storeKey, JSON.stringify(data.regions))
-    }
-    fetchRegions()
-  }, [maxDuration, minDuration, setRegions, url])
+      const settings = JSON.parse(settingsString)
+      const { values } = settings
+      if (values.url) {
+        setUrl(values.url)
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    catchException(() => {
+      const regionsString = localStorage.getItem("regions")
+      if (!regionsString) {
+        return
+      }
+      setRegions(JSON.parse(regionsString))
+    })
+  }, [])
 
   return (
     <>
       <h1>Phase loop player</h1>
-      <Input label="url" value={url} onChange={handleUrlChange} />
-      <Input
-        label="minDuration"
-        value={minDuration}
-        onChange={handleMinDurationChange}
-      />
-      <Input
-        label="maxDuration"
-        value={maxDuration}
-        onChange={handleMaxDurationChange}
-      />
+      <InputForm url={url} setUrl={setUrl} setRegions={setRegions} />
       <PhraseLoopPlayer url={url} regions={regions} />
     </>
   )
