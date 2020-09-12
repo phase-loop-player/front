b3908dcab7c65fa7f816ca6cee44172c6d2a91ff
yoyota <smarthorse6@gmail.com>
2020-09-12 17:20:46 +0900

    Rename to PhraseLoopPlayer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Container } from "react-bootstrap"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
-import { PhraseLoopPlayerContainer } from "./phraseLoopPlayer"
+import { PhraseLoopPlayerContainer } from "./PhraseLoopPlayer"
 
 function App() {
   return (
