4b145d8504e1c96196d2d8edc28480a1beb62ae2
yoyota <yoyota@users.noreply.github.com>
2020-10-04 10:52:48 +0000

    deploy: 2e7f251edb9ba7fb6cf4bbb7d6ca83b5268ad343

---
+++ precache-manifest.2ad099e4b7598a063d1080493443e5b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf90f3dc3fe5a2c914486c34a761957b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d22a5a0e3cf0a38f412e",
+    "url": "/static/css/2.5c30afb6.chunk.css"
+  },
+  {
+    "revision": "d22a5a0e3cf0a38f412e",
+    "url": "/static/js/2.63a3f2d1.chunk.js"
+  },
+  {
+    "revision": "54aacc676ddd0fe333f7e7f724e972ba",
+    "url": "/static/js/2.63a3f2d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ebb06ad60de74e82677",
+    "url": "/static/js/main.1c5507c5.chunk.js"
+  },
+  {
+    "revision": "fcdd4b24fab2f37520f5",
+    "url": "/static/js/runtime-main.3bb9ea61.js"
+  }
+]);
