151c0d3d7bc3e897dfd5f1d24a556af06b775a0a
yoyota <smarthorse6@gmail.com>
2020-10-04 16:47:45 +0900

    Add playback rate button

--- src/components/PhraseLoopPlayer/PhraseLoopPlayer.js
+++ src/components/PhraseLoopPlayer/PhraseLoopPlayer.js
@@ -2,7 +2,7 @@ import React, { useRef, useState, useEffect, useCallback } from "react"
 import "rc-slider/assets/index.css"
 import ReactPlayer from "react-player"
 import { GlobalHotKeys } from "react-hotkeys"
-import { Button } from "react-bootstrap"
+import { InputGroup, Button } from "react-bootstrap"
 import Slider from "rc-slider"
 
 import useLoop from "../../hooks/useLoop"
@@ -14,6 +14,7 @@ function PhraseLoopPlayer({ url, regions }) {
   const playerRef = useRef()
   const [player, setPlayer] = useState()
   const [duration, setDuration] = useState(0)
+  const [playbackRate, setPlaybackRate] = useState(1)
   const [videoState, setVideoState] = useState({ playedSeconds: 0 })
   const { onReady, onProgress } = useReactPlayerCallback({
     setPlayer,
@@ -65,6 +66,7 @@ function PhraseLoopPlayer({ url, regions }) {
         onProgress={onProgress}
         width="100%"
         height={window.innerHeight / 2}
+        playbackRate={playbackRate}
       />
       <Slider
         className="my-5"
@@ -81,13 +83,25 @@ function PhraseLoopPlayer({ url, regions }) {
         setLoopRegion={setLoopRegion}
         playedSeconds={videoState.playedSeconds}
       />
-      <div className="mt-5">
-        <Button size="lg" onClick={() => addLoopIndex(-1)}>
+      <div className="mt-5 d-flex">
+        <Button className="mr-1" onClick={() => addLoopIndex(-1)}>
           Previous
         </Button>
-        <Button size="lg" onClick={() => addLoopIndex(1)}>
+        <Button className="mr-1" onClick={() => addLoopIndex(1)}>
           Next
         </Button>
+        <InputGroup.Text className="mr-1">playback rate</InputGroup.Text>
+        {[0.25, 0.5, 0.75, 1, 1.25, 1.5].map(speed => (
+          <Button
+            variant="outline-secondary"
+            className="mr-1"
+            key={speed}
+            active={playbackRate === speed}
+            onClick={() => setPlaybackRate(speed)}
+          >
+            {speed}
+          </Button>
+        ))}
       </div>
       <p>{loopRegion.text}</p>
     </GlobalHotKeys>
